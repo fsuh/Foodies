4a1a33b51cb6598ff9fa7aade13dc387221b85c5
fsuh <92304761+fsuh@users.noreply.github.com>
2024-01-15 13:17:44 +0200

    add meals detail page

---
+++ app/meals/[mealSlug]/not-found.js
@@ -0,0 +1,9 @@
+const NotFoundError = () => {
+	return (
+		<main className="not-found">
+			<h1>Meal Not Found</h1>
+			<p>Unfortunately, we could not find the requested meal data or page</p>
+		</main>
+	);
+};
+export default NotFoundError;
